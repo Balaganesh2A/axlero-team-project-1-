7f99000a7989c8cc2d344d704051dae5803c18d2
Driti Parmar <dritiparmar@Dritis-MacBook-Air.local>
2026-07-18 14:17:06 +0530

    feat(dbt): add stg_superstore_orders_cleaned with type casting and filtering

---
+++ axlero_dbt/models/staging/stg_superstore_orders.sql
@@ -0,0 +1,23 @@
+select
+    row_id,
+    order_id,
+    order_date,
+    ship_date,
+    ship_mode,
+    customer_id,
+    customer_name,
+    segment,
+    country,
+    city,
+    state,
+    postal_code,
+    region,
+    product_id,
+    category,
+    sub_category,
+    product_name,
+    sales,
+    quantity,
+    discount,
+    profit
+from {{ source('raw', 'superstore_orders') }}
