02fbd2ae988f0d1051d8b0009ca070bdf5203b79
Devansh Srivastava <srivastavadevansh287@gmail.com>
2026-07-24 18:22:48 +0530

    Add query governance with queryRewrite limit

--- cube/cube.js
+++ cube/cube.js
@@ -1,4 +1,10 @@
 // Cube configuration options: https://cube.dev/docs/config
-/** @type{ import('@cubejs-backend/server-core').CreateOptions } */
+/** @type { import('@cubejs-backend/server-core').CreateOptions } */
 module.exports = {
+  queryRewrite: (query, { securityContext }) => {
+    if (!query.limit || query.limit > 5000) {
+      query.limit = 5000;
+    }
+    return query;
+  },
 };
